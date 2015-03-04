1255392b3c76f1da7bec5718205bc32bed36315d
David Braun <David.Braun@Toptal.com>
2015-03-04 11:57:31 -0800

    Set up for manual test.

--- cmd.sh
+++ cmd.sh
@@ -1,19 +1,25 @@
 #!/bin/sh
 
 cd ~/.decerver/source
-epm install --genesis contracts/genesis.json . helloworld
 
-# Now we need to tell the DApp about our chain and then we’re ready to VRoom.
+if [ -z $BLOCKCHAIN_ID ]; then
+  # Create our own blockchain.
+  
+  echo BLOCKCHAIN_ID not set, creating new chain.  
+  epm install --genesis contracts/genesis.json . helloworld
 
-blockchain_id=$(epm plop chainid)
-root_contract=$(epm plop vars | cut -d : -f 2)
+  # Now we need to tell the DApp about our chain and then we’re ready to VRoom.
 
-echo "Configuring package.json with blockchain_id ($blockchain_id) and "
-echo "root_contract ($root_contract)."
+  BLOCKCHAIN_ID=$(epm plop chainid)
+  ROOT_CONTRACT=$(epm plop vars | cut -d : -f 2)
+fi
+
+echo "Configuring package.json with BLOCKCHAIN_ID ($BLOCKCHAIN_ID) and "
+echo "ROOT_CONTRACT ($ROOT_CONTRACT)."
 
 mv package.json /tmp/
 
-jq '.module_dependencies[0].data |= . * {blockchain_id: "'$blockchain_id'", root_contract: "'$root_contract'"}' /tmp/package.json \
+jq '.module_dependencies[0].data |= . * {BLOCKCHAIN_ID: "'$BLOCKCHAIN_ID'", ROOT_CONTRACT: "'$ROOT_CONTRACT'"}' /tmp/package.json \
   > package.json
 
 # put the helloworld DApp in focus
--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+fig up -d helloworldchain
+helloworldchain=$(fig ps -q helloworldchain)
+export BLOCKCHAIN_ID=$(docker exec $helloworldchain epm plop chainid)
+export ROOT_CONTRACT=$(docker exec $helloworldchain epm plop vars | cut -d : -f 2)
+
 fig up -d seleniumnode
-fig run helloworldtest
+# fig run helloworldtest
